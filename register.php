0e6ef253e6edc71dd060230b6c8f7e224086744b
widespread0-0 <danielolugbule@gmail.com>
2025-04-24 16:25:30 +0100

    register.php

--- register.php
+++ register.php
@@ -22,3 +22,25 @@ if ($stmt->execute()) {
 } else {
     echo "Error: " . $stmt->error;
 }
+
+<!DOCTYPE html>
+<html>
+<head><title>Register</title></head>
+<body>
+
+<h1>Register</h1>
+<form method="POST" action="api/register.php">
+  <label>Name:</label><br>
+  <input type="text" name="name" required><br><br>
+
+  <label>Email:</label><br>
+  <input type="email" name="email" required><br><br>
+
+  <label>Password:</label><br>
+  <input type="password" name="password" required><br><br>
+
+  <button type="submit">Register</button>
+</form>
+
+</body>
+</html>
