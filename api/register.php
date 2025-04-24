c627a6ba6d28f042d73dddde4f68cdfbc9c48dc0
widespread0-0 <danielolugbule@gmail.com>
2025-04-24 18:00:30 +0100

    Update and rename register.php to api/register.php

--- api/register.php
+++ api/register.php
@@ -1,4 +1,3 @@
-//this is for api/register.php
 <?php
 require '../db.php';
 
@@ -24,25 +23,6 @@ if ($stmt->execute()) {
     echo "Error: " . $stmt->error;
 }
 
-// this is for register.php(html)
-<!DOCTYPE html>
-<html>
-<head><title>Register</title></head>
-<body>
-
-<h1>Register</h1>
-<form method="POST" action="api/register.php">
-  <label>Name:</label><br>
-  <input type="text" name="name" required><br><br>
-
-  <label>Email:</label><br>
-  <input type="email" name="email" required><br><br>
-
-  <label>Password:</label><br>
-  <input type="password" name="password" required><br><br>
-
-  <button type="submit">Register</button>
-</form>
 
 </body>
 </html>
