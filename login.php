aff2be2c0223208d62063b740e5cf5fe5bcb92d5
widespread0-0 <danielolugbule@gmail.com>
2025-04-24 18:04:25 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+<body>
+
+<h1>Login</h1>
+<form method="POST" action="api/login.php">
+  <label>Email:</label><br>
+  <input type="email" name="email" required><br><br>
+
+  <label>Password:</label><br>
+  <input type="password" name="password" required><br><br>
+
+  <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
