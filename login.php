e3f9971ec8063e99595cab4b59b5d6ca7d27867b
widespread0-0 <danielolugbule@gmail.com>
2025-04-24 16:26:12 +0100

    login.php

--- login.php
+++ login.php
@@ -16,3 +16,22 @@ if ($user && password_verify($password, $user['password'])) {
 } else {
     echo "Invalid login credentials.";
 }
+
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+<body>
+
+<h1>Login</h1>
+<form method="POST" action="api/login.php">
+  <label>Email:</label><br>
+  <input type="email" name="email" required><br><br>
+
+  <label>Password:</label><br>
+  <input type="password" name="password" required><br><br>
+
+  <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
